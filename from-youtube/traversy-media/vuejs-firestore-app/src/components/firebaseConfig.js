f87ff4180108f31438c4ba943b52cf0bda0e816c
Jedidiah Avelino <jed.avelino@gmail.com>
2017-12-15 16:21:27 +0800

    added vuejs-firebase-app

---
+++ from-youtube/traversy-media/vuejs-firestore-app/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyDvZQZhGtJ_toayEaStO3VDKCtyxZ1-58k",
+  authDomain: "vuefs-prod-20f41.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-20f41.firebaseio.com",
+  projectId: "vuefs-prod-20f41",
+  storageBucket: "vuefs-prod-20f41.appspot.com",
+  messagingSenderId: "459912196427"
+}
